034f9b94de0b9d196ce12354a22930cbc98c15fe
dunaevaa <32384871+dunaevaa@users.noreply.github.com>
2017-09-29 11:31:43 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,21 +2,20 @@
 using namespace std;
 int main (int argc, char* argv[])
 {
-cout <<"count:" <<argc-1<<endl;
-for (int i=1; i<=argc-1; i++)
- {
- cout <<"argv ["<<i<<"]:"<<argv [i]<<engl;
+  cout << "count:" << argc - 1 << endl;
+  for (int i=1; i <= argc - 1; i++) {
+      cout << "argv [" << i << "]:" << argv [i] << engl;
  }
-char operand;
-operand=argv [2] [0];
-switch (operand) {
-case '+':
-cout <<atoi (argv [1]) + atoi (argv [3])<<endl;
-break;
-case '-':
-cout <<atoi (argv [1]) - atoi (argv [3])<<endl;
-break;
-default:
-cerr<<"ERROR";
-}
+  char operand;
+  operand = argv [2] [0];
+  switch (operand) {
+      case '+':
+      cout << atoi (argv [1]) + atoi (argv [3]) << endl;
+      break;
+      case '-':
+      cout << atoi (argv [1]) - atoi (argv [3]) << endl;
+      break;
+      default:
+      cerr << "ERROR";
+  }
 }
